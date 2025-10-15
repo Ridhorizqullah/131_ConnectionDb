95198f3c1fdafb089b5b427263a74e8efc8d4e68
Ridhorzqullah <ridhorizqullah3@gmail.com>
2025-10-15 12:56:57 +0700

    penambahan get

--- index.js
+++ index.js
@@ -37,31 +37,5 @@ app.get('/mahasiswa', (req, res) => {
   });
 });
 
-// ✅ Tambahkan route POST
-app.post('/mahasiswa', (req, res) => {
-  const { id, nama, alamat, agama } = req.body;
 
-  if (!id || !nama || !alamat || !agama) {
-    return res.status(400).json({ message: "Semua field wajib diisi!" });
-  }
 
-  const query = "INSERT INTO biodata (id, nama, alamat, agama) VALUES (?, ?, ?, ?)";
-  db.query(query, [id, nama, alamat, agama], (err, result) => {
-    if (err) {
-      console.error("Error inserting data:", err);
-      res.status(500).send("Gagal menambahkan data");
-    } else {
-      res.status(201).json({ message: "Data berhasil ditambahkan", id: result.insertId });
-    }
-  });
-});
-
-// Routing sederhana
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-// Jalankan server
-app.listen(port, () => {
-  console.log(`🚀 Server is running at http://localhost:${port}`);
-});
